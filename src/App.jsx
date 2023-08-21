332b04b61edc43db3ff4c04469b961a3ca904219
Pranjal101Shrivastava <131521253+Pranjal101Shrivastava@users.noreply.github.com>
2023-08-21 20:46:36 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { fetchDataFromApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from './store/homeSlice';
 
-import Home from "./pages/home/home"
+import Home from "./pages/home/Home"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import Details from "./pages/details/Details"
@@ -19,14 +19,21 @@ function App() {
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
   
-  const apiTesting = () =>{
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig = () =>{
+    fetchDataFromApi('/configuration')
       .then((res) => {
         console.log(res);
-        dispatch(getApiConfiguration(res));
+
+        const url = {
+          backdrop : res.images.secure_base_url + "original",
+          poster : res.images.secure_base_url + "original",
+          profile : res.images.secure_base_url + "original",
+        };
+
+        dispatch(getApiConfiguration(url));
       });
   };
 
@@ -37,7 +44,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
-        <Route path="/search/query" element={<SearchResult />} />
+        <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<pageNotFound />} />
       </Routes>
